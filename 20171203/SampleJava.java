c3f2a425d14de4327b22800491f4e91ffc93abbb
ziyudom <tokushima@ziyudom.com>
2017-12-05 22:38:22 +0900

    なんの修正？

--- 20171203/SampleJava.java
+++ 20171203/SampleJava.java
@@ -1,13 +1,12 @@
 public class SampleJava{
-     public static void main(String[] args){
+  public static void main(String[] args){
 
-       StringBuilder hoge = new StringBuilder("test");
-       
-       //こっちのが速いもよう
-       for( int i = 0 ; i < 100 ; i++ ){
-          hoge.append("123");
-       }
-       String s = hoge.toString();
-       System.out.printf(s);
-   }
+    StringBuilder hoge = new StringBuilder("test");
+    //こっちのがふつうの文字列結合より速いもよう
+    for( int i = 0 ; i < 100 ; i++ ){
+      hoge.append("123");
+    }
+    String output = hoge.toString();
+    System.out.printf(output);
+  }
 }
