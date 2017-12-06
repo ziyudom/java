b6d226860e4bca51565f03e0a86e3c351ff2c08f
ziyudom <tokushima@ziyudom.com>
2017-12-06 18:26:28 +0900

    print/printf/printlnの違いについて

---
+++ 20171206/SampleJava.java
@@ -0,0 +1,26 @@
+public class SampleJava{
+  public static void main(String[] args){
+    System.out.print("test");
+    System.out.print("からのtest");
+    System.out.print("からのtest3");
+    //改行もかねて空白のln
+    System.out.println();
+    System.out.println("println");
+    System.out.println("println2");
+    System.out.println("println3");
+    System.out.println("println4");
+    //改行もかねて空白のln
+    System.out.println();
+    System.out.printf("只今の所持金%5d円",55);
+  }
+}
+
+/* 出力
+testからのtestからのtest3
+println
+println2
+println3
+println4
+
+只今の所持金   55円
+*/
