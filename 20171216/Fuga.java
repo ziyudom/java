f253bc056aed7d1bdcac135e8f127064d89596db
ziyudom <tokushima@ziyudom.com>
2017-12-16 23:56:24 +0900

    jarファイル作製

---
+++ 20171216/Fuga.java
@@ -0,0 +1,5 @@
+public class Fuga {
+    public static void main(String[] args){
+        System.out.println("fuga");
+    }
+}
---
+++ 20171216/Hoge.java
@@ -0,0 +1,5 @@
+public class Hoge {
+    public static void main(String[] args){
+        System.out.println("hoge");
+    }
+}
