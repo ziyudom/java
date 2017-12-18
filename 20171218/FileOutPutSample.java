41ea9faf60fb16eb397d7e725ea5279f14b38cde
ziyudom <tokushima@ziyudom.com>
2017-12-18 19:16:06 +0900

    なんでも無い

--- 20171218/FileOutPutSample.java
+++ 20171218/FileOutPutSample.java
@@ -3,10 +3,10 @@ import java.io.*;
 public class FileOutPutSample {
     public static void main(String[] args){
         try(FileOutputStream fos = new FileOutputStream("out.txt",true);){
-        byte b1 = 74; //ASCII：(J)
-		byte b2 = 65; //ASCII：(A)
-		byte b3 = 86; //ASCII：(V)
-		byte b4 = 65; //ASCII：(A)
+        byte b1 = 99;
+		byte b2 = 65;
+		byte b3 = 86;
+		byte b4 = 65;
 
 		fos.write(b1);
 		fos.write(b2);
